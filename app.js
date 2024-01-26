b36268c4713a5508b3615198dda32bf95456cc93
tiagoJC1 <tiagocuello679@gmail.com>
2024-01-25 22:55:34 -0300

    subo rama 3, perdon por el nombre incompleto

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(express.static('public'));
 app.get('/', (req,res) =>
 res.sendFile(path.join(__dirname,'/views/home.html')));
 
-app.get('/prueba', (req,res) =>
-res.sendFile(path.join(__dirname,'/views/prueba.html')));
+app.get('/register', (req,res) =>
+res.sendFile(path.join(__dirname,'/views/register.html')));
 
 app.get("/home", (req, res) => {
     res.redirect('/');
