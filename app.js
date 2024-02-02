34c4c447541dfc6b76e2580f59082a3d49abd7e1
tiagoJC1 <tiagocuello679@gmail.com>
2024-02-02 02:22:02 -0300

    subo la parte numero 5 de mercado liebre

--- app.js
+++ app.js
@@ -11,14 +11,24 @@ app.get('/', (req,res) =>
 res.sendFile(path.join(__dirname,'/views/home.html')));
 
 app.get('/register', (req,res) =>
-res.sendFile(path.join(__dirname,'/views/register.html')));
+res.sendFile(path.join(__dirname,'/views/register.html'))); 
 
+app.get('/login', (req,res) => 
+res.sendFile(path.join(__dirname,'/views/login.html')));
+
+
+app.get("/register", (req, res) => {
+  res.redirect('/register');
+});
 app.get("/home", (req, res) => {
     res.redirect('/');
   });
-app.get("/prueba", (req, res) => {
-    res.redirect('/prueba');
-  });  
+
+
+
+// app.get("/login", (req, res) => {
+//     res.redirect('/login');
+//   });  
 
 app.listen(port , () =>
 console.log(`http://localhost:${port}`));
